9141a54d41f5a2ffd316dd278d5e77a73cb0a522
xiezhipeng.peng <xiezhipeng.peng@bytedance.com>
2023-04-15 11:41:27 +0800

    测试可写事件、非阻塞写

---
+++ third-java-nio/src/main/java/com/peng/thirdjavanio/test/block/Client.java
@@ -0,0 +1,88 @@
+package com.peng.thirdjavanio.test.block;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * @author xiezhipeng <xiezhipeng.peng@bytedance.com>
+ * @Date 2023/04/15
+ */
+@Slf4j
+public class Client {
+    public static void main(String[] args) throws IOException {
+        SocketChannel socketChannel = SocketChannel.open();
+        socketChannel.configureBlocking(false);
+        socketChannel.connect(new InetSocketAddress("127.0.0.1", 9001));
+
+        Selector selector = Selector.open();
+        SelectionKey key = socketChannel.register(selector, SelectionKey.OP_CONNECT);
+
+        // 创建要发送的数据
+        StringBuilder sb = new StringBuilder();
+        // 1MB的数据 TODO 可以适当增大或缩小发送数据量，控制发送次数
+        for (int i = 0; i < 1024 * 1024 * 1; i++) {
+            sb.append("a");
+        }
+        // 此处返回的ByteBuffer已经是「读模式」了，不用再调用flip方法
+        ByteBuffer byteBuffer = Charset.defaultCharset().encode(sb.toString());
+        // 将要写的数据绑定到selectionKey上
+        key.attach(byteBuffer);
+
+        int writeCount = 0;
+
+        log.info("start write, position:{}, limit:{}, capacity:{}", byteBuffer.position(), byteBuffer.limit(), byteBuffer.capacity());
+        while (true) {
+            int select = selector.select();
+            if (select <= 0) {
+                continue;
+            }
+
+            Set<SelectionKey> selectedKeys = selector.selectedKeys();
+            Iterator<SelectionKey> keyIterator = selectedKeys.iterator();
+            while (keyIterator.hasNext()) {
+                SelectionKey selectionKey = keyIterator.next();
+                keyIterator.remove();
+                if (selectionKey.isConnectable()) {
+                    // TODO 为啥必须调用finishConnect方法程序才正常？
+                    log.info("connect success....{}", ((SocketChannel) selectionKey.channel()).finishConnect());
+                    selectionKey.interestOps(SelectionKey.OP_WRITE);
+                    continue;
+                }
+
+                if (selectionKey.isWritable()) {
+                    writeCount++;
+                    // 获取需要写的数据
+                    ByteBuffer buffer = (ByteBuffer) selectionKey.attachment();
+                    log.info("write round {} start, position:{}, limit:{}, capacity:{}", writeCount, buffer.position(), buffer.limit(), buffer.capacity());
+                    // 如果有剩余的没写完继续写
+                    if (buffer.hasRemaining()) {
+                        SocketChannel sc = (SocketChannel) selectionKey.channel();
+                        // 非阻塞写，当发送缓冲区满了就不可写了，此时会直接返回；若是阻塞IO，当发送缓冲区满了，写会阻塞直到所有数据都写到缓冲区
+                        int write = sc.write(buffer);
+                        log.info("write round {} end, writeData:{}, position:{}, limit:{}, capacity:{}", writeCount, write, buffer.position(), buffer.limit(), buffer.capacity());
+                    } else {
+                        log.info("write round {} finished", writeCount);
+                        // TODO 一定要cancel吗？
+//                        selectionKey.cancel();
+                        socketChannel.close();
+                    }
+                }
+
+            }
+        }
+    }
+
+    public static void printBufferInfo(Buffer buffer, int writeRound) {
+        log.info("writeRound:{}, position:{}, limit:{}, capacity:{}", writeRound, buffer.position(), buffer.limit(), buffer.capacity());
+    }
+}
---
+++ third-java-nio/src/main/java/com/peng/thirdjavanio/test/block/Server.java
@@ -0,0 +1,89 @@
+package com.peng.thirdjavanio.test.block;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * @author xiezhipeng <xiezhipeng.peng@bytedance.com>
+ * @Date 2023/04/15
+ */
+@Slf4j
+public class Server {
+    public static void main(String[] args) throws IOException {
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.configureBlocking(false);
+        serverSocketChannel.bind(new InetSocketAddress("127.0.0.1", 9001));
+        Selector selector = Selector.open();
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+        int readCount = 0;
+        log.info("start");
+        while (true) {
+            int select = selector.select();
+            if (select <= 0) {
+                continue;
+            }
+
+            Set<SelectionKey> selectedKeys = selector.selectedKeys();
+            Iterator<SelectionKey> keysIterator = selectedKeys.iterator();
+            while (keysIterator.hasNext()) {
+                SelectionKey selectionKey = keysIterator.next();
+                keysIterator.remove();
+
+                if (selectionKey.isAcceptable()) {
+                    SocketChannel socketChannel = ((ServerSocketChannel) selectionKey.channel()).accept();
+                    if (socketChannel == null) {
+                        log.info("accept null");
+                        continue;
+                    }
+
+                    log.info("accept success:{}, selectorSame:{}", socketChannel.getRemoteAddress(), selector == selectionKey.selector());
+                    socketChannel.configureBlocking(false);
+                    SelectionKey newKey = socketChannel.register(selectionKey.selector(), SelectionKey.OP_READ);
+                    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                    // 绑定一个ByteArrayOutputStream，将该SocketChannel读到的数据都写到byteArrayOutputStream中
+                    newKey.attach(byteArrayOutputStream);
+                    continue;
+                }
+                if (selectionKey.isReadable()) {
+                    readCount++;
+                    log.info("read round {} start", readCount);
+                    SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+                    // TODO 可以适当增大或缩小Buffer，控制读取次数
+                    ByteBuffer byteBuffer = ByteBuffer.allocate(1024 * 128);
+                    int readLen = socketChannel.read(byteBuffer);
+
+                    if (readLen == -1) {
+                        // 输出读取到的一部分数据
+                        ByteArrayOutputStream outputStream = (ByteArrayOutputStream) selectionKey.attachment();
+                        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(outputStream.toByteArray());
+                        byte[] readBuffer = new byte[100];
+                        byteArrayInputStream.read(readBuffer);
+                        log.info("read round {} finished, dataSize:{}, data:{}", readCount, readBuffer.length, new String(readBuffer));
+                        socketChannel.close();
+                    } else if (readLen == 0) {
+                        log.info("read nothing");
+                        continue;
+                    } else {
+                        byte[] data = byteBuffer.array();
+                        // 将读到的数据往输出流中写
+                        ByteArrayOutputStream outputStream = (ByteArrayOutputStream) selectionKey.attachment();
+                        outputStream.write(data);
+                        log.info("read round {} end, curReadData:{}, readAllData:{}", readCount, data.length, outputStream.size());
+                    }
+                }
+            }
+        }
+    }
+}
